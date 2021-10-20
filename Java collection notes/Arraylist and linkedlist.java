12b78cb25950381cb4c6a57fbb80f0e6d9ff8ba6
Kiran S <kiranpintu633@gmail.com>
2021-10-20 15:07:47 +0530

    JC notes

---
+++ Java collection notes/Arraylist and linkedlist.java
@@ -0,0 +1,22 @@
+ArrayList - group of individual object/element as one entity 
+          - hetrogeneous element allowed
+          - duplicates are alloed 
+          - Insertion order is preserved 
+          - objects/elements stored in continues memory locations.
+          - Indexed collection only
+          - ArrayList List - growable collection object 
+
+LinkedList - group of individual object/element as one entity 
+           - hetrogeneous element allowed
+           - duplicates are alloed 
+           - Insertion order is preserved 
+
+LinkedList - Where elemetns are not stored in continoues memory locations.
+
+? How the elemnets are stored. (node)
+? node?
+class LinkedList impliments List, Deque {
+addFirst()
+addLast()
+removeFirst()
+removeLast()
